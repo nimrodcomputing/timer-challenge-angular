687f8866f5320fcb169988dc25988f2869e75c3c
William Hunter <wrh@nimrodcomputing.co.uk>
2018-11-20 09:17:15 +0000

    Add a few tests and make sure the generated tests work

--- TimerChallenge/ClientApp/src/app/app.component.spec.ts
+++ TimerChallenge/ClientApp/src/app/app.component.spec.ts
@@ -1,31 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {TimerComponent} from "./timer/timer.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent,
+                TimerComponent
+            ],
+            imports: [
+                FormsModule,
+                HttpClientTestingModule
+            ]
+        }).compileComponents();
+    }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have as title 'timer-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('timer-challenge');
-  });
+    it(`should have as title 'timer-challenge'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('timer-challenge');
+    });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to timer-challenge!');
-  });
 });
--- TimerChallenge/ClientApp/src/app/timer/timer.component.spec.ts
+++ TimerChallenge/ClientApp/src/app/timer/timer.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TimerComponent } from './timer.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TimerComponent', () => {
   let component: TimerComponent;
@@ -8,7 +10,11 @@ describe('TimerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TimerComponent ]
+      declarations: [ TimerComponent ],
+        imports: [
+            FormsModule,
+            HttpClientTestingModule
+        ]
     })
     .compileComponents();
   }));
@@ -22,4 +28,18 @@ describe('TimerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should forbid values below 121', () => {
+        component.timer = 120;
+        let valid = component.validateTimer();
+        expect(valid).toBeFalsy();
+        expect(component.validInput).toBeFalsy();
+    });
+
+    it('should allow values above 120', () => {
+        component.timer = 121;
+        let valid = component.validateTimer();
+        expect(valid).toBeTruthy();
+        expect(component.validInput).toBeTruthy();
+    });
 });
